c7d77de2631608c9020b3451aef6348cb1e0c39d
Juan Restrepo <jcrestrepobedoya@gmail.com>
2018-11-29 21:30:38 -0500

    renames

--- testConfig.py
+++ testConfig.py
@@ -1,11 +1,11 @@
 import os
 
-driverRelPath='/googleTranslate/resources/drivers/chromedriver'
+DEFAULT_DRIVER='/googleTranslate/resources/drivers/chromedriver'
 
 config = {
     'BASE_DIR':os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
     'DEFAULT_URL':'https://www.google.com/',
-    'WEB_DRIVER': f'{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}{driverRelPath}'
+    'WEB_DRIVER': f'{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}{DEFAULT_DRIVER}'
 }
 
 if __name__=="__main__":
